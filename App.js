f31a4654218c04e0440d039f8df5ceff91409c43
davidjumble <hellodavidparkinson@gmail.com>
2018-07-26 11:26:59 +0100

    need to play with new playback controller that doud'nt rely on status

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import React from "react";
 import {
   Dimensions,
   Image,
-  Slider,
   StyleSheet,
   Text,
   TouchableHighlight,
@@ -28,48 +27,17 @@ const ICON_RECORD_BUTTON = new Icon(
   70,
   119
 );
-const ICON_RECORDING = new Icon(
-  require("./assets/images/record_icon.png"),
-  20,
-  14
-);
 
 const ICON_PLAY_BUTTON = new Icon(
   require("./assets/images/play_button.png"),
   34,
   51
 );
-const ICON_PAUSE_BUTTON = new Icon(
-  require("./assets/images/pause_button.png"),
-  34,
-  51
-);
-const ICON_STOP_BUTTON = new Icon(
-  require("./assets/images/stop_button.png"),
-  22,
-  22
-);
-
-const ICON_MUTED_BUTTON = new Icon(
-  require("./assets/images/muted_button.png"),
-  67,
-  58
-);
-const ICON_UNMUTED_BUTTON = new Icon(
-  require("./assets/images/unmuted_button.png"),
-  67,
-  58
-);
-
-const ICON_TRACK_1 = new Icon(require("./assets/images/track_1.png"), 166, 5);
-const ICON_THUMB_1 = new Icon(require("./assets/images/thumb_1.png"), 18, 19);
-const ICON_THUMB_2 = new Icon(require("./assets/images/thumb_2.png"), 15, 19);
 
 const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get("window");
 const BACKGROUND_COLOR = "#FFF8ED";
-const LIVE_COLOR = "#FF0000";
+
 const DISABLED_OPACITY = 0.5;
-const RATE_SCALE = 3.0;
 
 export default class App extends React.Component {
   constructor(props) {
@@ -91,7 +59,7 @@ export default class App extends React.Component {
       isPlaying: false,
       isRecording: false,
       fontLoaded: false,
-      shouldCorrectPitch: true,
+
       volume: 1.0,
       rate: 1.0
     };
@@ -119,17 +87,113 @@ export default class App extends React.Component {
     });
   };
 
+  async _stopPlaybackAndBeginRecording() {
+    this.setState({
+      isLoading: true
+    });
+    //clears current sound file if there is one
+    if (this.sound !== null) {
+      await this.sound.unloadAsync();
+      this.sound.setOnPlaybackStatusUpdate(null);
+      this.sound = null;
+    }
+    //audio settings
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: true,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
+    });
+    //clears current recording file
+    if (this.recording !== null) {
+      this.recording.setOnRecordingStatusUpdate(null);
+      this.recording = null;
+    }
+
+    //new instance of a recording class
+    const recording = new Audio.Recording();
+    await recording.prepareToRecordAsync(this.recordingSettings);
+    //sets a function to be called regularly with the status of the recording
+    recording.setOnRecordingStatusUpdate(this._updateScreenForRecordingStatus);
+
+    this.recording = recording;
+    //startAsync starts the recording
+    await this.recording.startAsync(); // Will call this._updateScreenForRecordingStatus to update the screen.
+    //
+    //won't move on to set state as false until recording done
+    //then stop recording and enable playback is called
+    this.setState({
+      isLoading: false
+    });
+    console.log("recording");
+  }
+
+  async downloadNewSounds() {
+    await FileSystem.downloadAsync(
+      "https://s3.eu-west-2.amazonaws.com/soundtown.pitched.audio/C3.mp3",
+      FileSystem.documentDirectory + "C3.mp3"
+    )
+      .then(({ uri }) => {
+        console.log("Finished downloading to ", uri);
+        this.soundURI = uri;
+        console.log("dddddddddddd", this.soundURI);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  async _stopRecordingAndEnablePlayback() {
+    this.setState({
+      isLoading: true
+    });
+    try {
+      //this stops the recording
+      //also setRecordingStatusUpdate calls updateScreenForRecordingStatus one last Time
+      await this.recording.stopAndUnloadAsync();
+    } catch (error) {
+      // Do nothing -- we are already unloaded.
+    }
+    let uri = await this.recording.getURI();
+
+    await this.uploadAudioAsync(uri);
+    // sound will upload and then it will save new sounds using
+    await this.downloadNewSounds();
+
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS,
+      playsInSilentModeIOS: true,
+      playsInSilentLockedModeIOS: true,
+      shouldDuckAndroid: true,
+      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
+    });
+
+    //leaving this in as a way of testing that a recording has been made
+    const { sound, status } = await this.recording.createNewLoadedSound(
+      {
+        isLooping: true,
+        isMuted: this.state.muted,
+        volume: this.state.volume,
+        rate: this.state.rate,
+        shouldCorrectPitch: this.state.shouldCorrectPitch
+      },
+      this._updateScreenForSoundStatus
+    );
+    this.sound = sound;
+
+    this.setState({
+      isLoading: false
+    });
+  }
+
   _updateScreenForSoundStatus = status => {
     if (status.isLoaded) {
       this.setState({
-        soundDuration: status.durationMillis,
-        soundPosition: status.positionMillis,
         shouldPlay: status.shouldPlay,
         isPlaying: status.isPlaying,
-        rate: status.rate,
-        muted: status.isMuted,
-        volume: status.volume,
-        shouldCorrectPitch: status.shouldCorrectPitch,
+
         isPlaybackAllowed: true
       });
     } else {
@@ -145,12 +209,14 @@ export default class App extends React.Component {
   };
 
   _updateScreenForRecordingStatus = status => {
+    //conditional that keeps track of whether recording is a happening in state
     if (status.canRecord) {
       this.setState({
-        isRecording: status.isRecording,
-        recordingDuration: status.durationMillis
+        isRecording: status.isRecording
       });
-    } else if (status.isDoneRecording) {
+    }
+    //changes state once recording status is finished
+    else if (status.isDoneRecording) {
       this.setState({
         isRecording: false,
         recordingDuration: status.durationMillis
@@ -161,110 +227,9 @@ export default class App extends React.Component {
     }
   };
 
-  async _stopPlaybackAndBeginRecording() {
-    this.setState({
-      isLoading: true
-    });
-    if (this.sound !== null) {
-      await this.sound.unloadAsync();
-      this.sound.setOnPlaybackStatusUpdate(null);
-      this.sound = null;
-    }
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: true,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      playsInSilentModeIOS: true,
-      shouldDuckAndroid: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
-    });
-    if (this.recording !== null) {
-      this.recording.setOnRecordingStatusUpdate(null);
-      this.recording = null;
-    }
-
-    //new instance of a recording class
-    const recording = new Audio.Recording();
-    await recording.prepareToRecordAsync(this.recordingSettings);
-    recording.setOnRecordingStatusUpdate(this._updateScreenForRecordingStatus);
-
-    this.recording = recording;
-    //startAsync starts the recording
-    await this.recording.startAsync(); // Will call this._updateScreenForRecordingStatus to update the screen.
-    this.setState({
-      isLoading: false
-    });
-    console.log("recording");
-  }
-
-  async _stopRecordingAndEnablePlayback() {
-    this.setState({
-      isLoading: true
-    });
-    try {
-      //this stops the recording
-      await this.recording.stopAndUnloadAsync();
-    } catch (error) {
-      // Do nothing -- we are already unloaded.
-    }
-    await FileSystem.downloadAsync(
-      "https://s3.eu-west-2.amazonaws.com/soundtown.pitched.audio/C3.mp3",
-      FileSystem.documentDirectory + "C3.mp3"
-    )
-      .then(({ uri }) => {
-        console.log("Finished downloading to ", uri);
-        this.soundURI = uri;
-        console.log("dddddddddddd", this.soundURI);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-
-    let uri = await this.recording.getURI();
-    //add a .then to this once we're ready
-    await this.uploadAudioAsync(uri);
-
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS,
-      playsInSilentModeIOS: true,
-      playsInSilentLockedModeIOS: true,
-      shouldDuckAndroid: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
-    });
-
-    const { sound, status } = await this.recording.createNewLoadedSound(
-      {
-        isLooping: true,
-        isMuted: this.state.muted,
-        volume: this.state.volume,
-        rate: this.state.rate,
-        shouldCorrectPitch: this.state.shouldCorrectPitch
-      },
-      this._updateScreenForSoundStatus
-    );
-    this.sound = sound;
-
-    // const soundObject = new Expo.Audio.Sound();
-    // try {
-    //   await soundObject.loadAsync(this.soundURI);
-
-    //   // Your sound is playing!
-    // } catch (error) {
-    //   // An error occurred!
-    // }
-    // console.log(soundObject);
-    // soundObject.playAsync();
-    // this.sound = soundObject;
-
-    this.setState({
-      isLoading: false
-    });
-  }
-
   async uploadAudioAsync(uri) {
     //console.log("Uploading " + uri);e
-    let apiUrl =
-      "https://t9kfovb7kk.execute-api.eu-west-2.amazonaws.com/dev/api/sample";
+    let apiUrl = "https://soundtown-dev.herokuapp.com/api/sample";
     let uriParts = uri.split(".");
     let fileType = uriParts[uriParts.length - 1];
 
@@ -320,7 +285,6 @@ export default class App extends React.Component {
     this.playSound();
 
     console.log("playing");
-
   };
 
   render() {
@@ -417,130 +381,6 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  emptyContainer: {
-    alignSelf: "stretch",
-    backgroundColor: BACKGROUND_COLOR
-  },
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-    backgroundColor: BACKGROUND_COLOR,
-    minHeight: DEVICE_HEIGHT,
-    maxHeight: DEVICE_HEIGHT
-  },
-  noPermissionsText: {
-    textAlign: "center"
-  },
-  wrapper: {},
-  halfScreenContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-    minHeight: DEVICE_HEIGHT / 2.0,
-    maxHeight: DEVICE_HEIGHT / 2.0
-  },
-  recordingContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-    minHeight: ICON_RECORD_BUTTON.height,
-    maxHeight: ICON_RECORD_BUTTON.height
-  },
-  recordingDataContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    minHeight: ICON_RECORD_BUTTON.height,
-    maxHeight: ICON_RECORD_BUTTON.height,
-    minWidth: ICON_RECORD_BUTTON.width * 3.0,
-    maxWidth: ICON_RECORD_BUTTON.width * 3.0
-  },
-  recordingDataRowContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    minHeight: ICON_RECORDING.height,
-    maxHeight: ICON_RECORDING.height
-  },
-  playbackContainer: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-    alignSelf: "stretch",
-    minHeight: ICON_THUMB_1.height * 2.0,
-    maxHeight: ICON_THUMB_1.height * 2.0
-  },
-  playbackSlider: {
-    alignSelf: "stretch"
-  },
-  liveText: {
-    color: LIVE_COLOR
-  },
-  recordingTimestamp: {
-    paddingLeft: 20
-  },
-  playbackTimestamp: {
-    textAlign: "right",
-    alignSelf: "stretch",
-    paddingRight: 20
-  },
-  image: {
-    backgroundColor: BACKGROUND_COLOR
-  },
-  textButton: {
-    backgroundColor: BACKGROUND_COLOR,
-    padding: 10
-  },
-  buttonsContainerBase: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between"
-  },
-  buttonsContainerTopRow: {
-    maxHeight: ICON_MUTED_BUTTON.height,
-    alignSelf: "stretch",
-    paddingRight: 20
-  },
-  playStopContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    minWidth: ((ICON_PLAY_BUTTON.width + ICON_STOP_BUTTON.width) * 3.0) / 2.0,
-    maxWidth: ((ICON_PLAY_BUTTON.width + ICON_STOP_BUTTON.width) * 3.0) / 2.0
-  },
-  volumeContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    minWidth: DEVICE_WIDTH / 2.0,
-    maxWidth: DEVICE_WIDTH / 2.0
-  },
-  volumeSlider: {
-    width: DEVICE_WIDTH / 2.0 - ICON_MUTED_BUTTON.width
-  },
-  buttonsContainerBottomRow: {
-    maxHeight: ICON_THUMB_1.height,
-    alignSelf: "stretch",
-    paddingRight: 20,
-    paddingLeft: 20
-  },
-  rateSlider: {
-    width: DEVICE_WIDTH / 2.0
-  }
-});
+const styles = StyleSheet.create({});
 
 Expo.registerRootComponent(App);
