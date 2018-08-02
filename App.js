29b439f4aa9c15a2ce670e8d9bc705583782557e
giqbal <gohars.iqbal@gmail.com>
2018-08-02 09:59:53 +0100

    Refactor record functionality

--- App.js
+++ App.js
@@ -1,30 +1,25 @@
 import React, { Component } from "react";
 import {
-  Dimensions,
-  Image,
-  StyleSheet,
   Text,
-  TouchableHighlight,
   View,
   ScrollView,
   ImageBackground
 } from "react-native";
-import { Asset, Audio, FileSystem, Font, Permissions } from "expo";
+import { Audio, Font, Permissions } from "expo";
 import styles from "./styles";
 import RecordButtons from "./components/record-buttons";
 import Buttons from "./components/play-buttons";
+import { configureAudioMode} from './utils'
 
 class App extends Component {
   constructor() {
     super();
-    this.recording = null;
     this.toneSoundObjs = {};
 
     this.state = {
       isRecording: false,
       isLoading: false,
       soundsReady: false,
-      isPlaying: false,
       fontLoaded: false,
       haveRecordingPermissions: false
     };
@@ -37,25 +32,12 @@ class App extends Component {
       });
       this.setState({ fontLoaded: true });
     })();
-    this.askForPermissions();
+    this.getPermission();
   }
 
   render() {
     return !this.state.fontLoaded ? (
       <View style={styles.emptyContainer} />
-    ) : !this.state.haveRecordingPermissions ? (
-      <View style={styles.container}>
-        <View />
-        <Text
-          style={[
-            styles.noPermissionsText,
-            { fontFamily: "cutive-mono-regular" }
-          ]}
-        >
-          You must enable audio recording permissions in order to use this app.
-        </Text>
-        <View />
-      </View>
     ) : (
       <ScrollView>
         <ImageBackground
@@ -64,27 +46,39 @@ class App extends Component {
           resizeMode="contain"
         >
           <RecordButtons
-            _onRecordPressed={this._onRecordPressed}
             isLoading={this.state.isLoading}
-            isRecording={this.state.isRecording}
+            updateIsLoading={this.updateIsLoading}
+            updateSoundsReady={this.updateSoundsReady}
+            getTones={this.getTones}
           />
           <Buttons
             toneSoundObjs={this.toneSoundObjs}
             isLoading={this.state.isLoading}
             canPlay={this.state.soundsReady}
-            isPlaying={this.state.isPlaying}
           />
         </ImageBackground>
       </ScrollView>
     );
   }
 
-  askForPermissions = async () => {
-    const response = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-    this.setState({
-      haveRecordingPermissions: response.status === "granted"
-    });
+  getPermission = () => {
+    if (!this.state.haveRecordingPermissions) {
+      Permissions.askAsync(Permissions.AUDIO_RECORDING)
+      .then(res => {
+        this.setState({
+          haveRecordingPermissions: res.status === "granted"
+        });
+      });
+    }
   };
+
+  updateIsLoading = (bool) => {
+    this.setState({isLoading: bool})
+  }
+
+  updateSoundsReady = (bool) => {
+    this.setState({soundsReady: bool})
+  }
 
   getTones = uri => {
     const fileType = "caf";
@@ -111,107 +105,11 @@ class App extends Component {
         console.log(convertedTones);
         for (let tone in convertedTones) {
           this.toneSoundObjs[tone] = new Audio.Sound();
-          this.toneSoundObjs[tone].setOnPlaybackStatusUpdate(
-            this.updateIsPlayingState
-          );
           this.toneSoundObjs[tone].loadAsync({ uri: convertedTones[tone] });
         }
+        configureAudioMode('playback');
         this.setState({ isLoading: false, soundsReady: true });
       });
-  };
-
-  _onRecordPressed = () => {
-    if (this.state.isRecording) {
-      this._stopRecordingAndEnablePlayback();
-    } else {
-      this._stopPlaybackAndBeginRecording();
-    }
-  };
-
-  async _stopPlaybackAndBeginRecording() {
-    this.setState({
-      isRecording: true,
-      soundsReady: false
-    });
-    //clears current sound file if there is one
-    for (let tone in this.toneSoundObjs) {
-      this.toneSoundObjs[tone].unloadAsync();
-    }
-    this.toneSoundObjs = {};
-    //audio settings
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: true,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      playsInSilentModeIOS: true,
-      shouldDuckAndroid: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
-    });
-    //clears current recording file
-
-    // this.recording.setOnRecordingStatusUpdate(null);
-    // this.recording = null;
-
-    if (this.recording !== null) {
-      this.recording.setOnRecordingStatusUpdate(null);
-      this.recording = null;
-    }
-
-    const recordingSettings = JSON.parse(
-      JSON.stringify(Audio.RECORDING_OPTIONS_PRESET_LOW_QUALITY)
-    );
-
-    //new instance of a recording class
-    const recording = new Audio.Recording();
-    await recording.prepareToRecordAsync(recordingSettings);
-    //sets a function to be called regularly with the status of the recording
-    // recording.setOnRecordingStatusUpdate(this._updateScreenForRecordingStatus);
-
-    this.recording = recording;
-    //startAsync starts the recording
-    await this.recording.startAsync(); // Will call this._updateScreenForRecordingStatus to update the screen.
-    //
-    //won't move on to set state as false until recording done
-    //then stop recording and enable playback is called
-
-    console.log("recording");
-  }
-
-  async _stopRecordingAndEnablePlayback() {
-    this.setState({
-      isLoading: true,
-      isRecording: false
-    });
-    try {
-      //this stops the recording
-      //also setRecordingStatusUpdate calls updateScreenForRecordingStatus one last Time
-      await this.recording.stopAndUnloadAsync();
-    } catch (error) {
-      // Do nothing -- we are already unloaded.
-    }
-    let uri = await this.recording.getURI();
-
-    //UNCOMMENT THIS OUT ONCE WE ARE READY TO SEND FILES EVERY TIMER
-
-    //await this.uploadAudioAsync(uri);
-    // sound will upload and then it will save new sounds using
-
-    await Audio.setAudioModeAsync({
-      allowsRecordingIOS: false,
-      interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_MIX_WITH_OTHERS,
-      playsInSilentModeIOS: true,
-      playsInSilentLockedModeIOS: true,
-      shouldDuckAndroid: true,
-      interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX
-    });
-
-    //leaving this in as a way of testing that a recording has been made
-
-    this.getTones(uri);
-  }
-
-  updateIsPlayingState = playbackStatus => {
-    if (playbackStatus.isPlaying) this.setState({ isPlaying: true });
-    else this.setState({ isPlaying: false });
   };
 }
 
